7631e0cf461b03b2374ef04c760fca8eb561d3fe
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-02-29 22:52:16 +0300

    add reactFragment

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     console.log(selectedTopic);
   }
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -66,7 +66,7 @@ function App() {
           )}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
